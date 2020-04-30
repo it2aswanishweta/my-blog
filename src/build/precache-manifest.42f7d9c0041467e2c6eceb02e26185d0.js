a6d397ab4d4d5b594a3a4242fe79bd737e814fee
Shweta <it2aswanishweta@gmail.com>
2020-04-30 03:37:00 -0400

    First commit

---
+++ src/build/precache-manifest.42f7d9c0041467e2c6eceb02e26185d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ea7d79d19a203be5f211a96f476bbae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11a2f6991e89e8ace131",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "4add875e6da769712842",
+    "url": "/static/js/2.f3bda445.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f3bda445.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11a2f6991e89e8ace131",
+    "url": "/static/js/main.8da4b771.chunk.js"
+  },
+  {
+    "revision": "56e9b91662fe3e442b95",
+    "url": "/static/js/runtime-main.35fed2a9.js"
+  }
+]);
